2e43605d0d17a9bd670b7131ca84d0812a5f7344
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-05-24 21:01:53 -0300

    create views

--- challenge_image_gallery/src/components/Image.tsx
+++ challenge_image_gallery/src/components/Image.tsx
@@ -1,5 +1,3 @@
-
-
 const Image = (props:any) => {
     return ( 
         <>
--- challenge_image_gallery/src/components/Images.tsx
+++ challenge_image_gallery/src/components/Images.tsx
@@ -1,10 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from 'react';
 import Image from './Image'
+import { ImageContext } from '../context/ImageProvider';
 
 function Images() {
 
+    const [images, setImages] = useState(Array<any>)
+
+    const keyword = useContext(ImageContext)
+
+    useEffect(() => {
+        fetch(`https://api.unsplash.com/search/photos?query=${keyword}&client_id=${'4EnYaxbE-DFhiGP6HRgwPrMiYdt0U1Zb8H0nPb0qdNU'}`)
+            .then(res => res.json())
+            .then(data => setImages(data.results))
+    }, [])
+
     return (
-        <div className="w-[80%] grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 my-2 mx-auto">
+        <div className="w-[80%] grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 my-2 mx-auto">            
             {(images && images.map(e => <Image img={e} />))}
         </div>
     );
