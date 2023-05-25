4aedb5fdff6b61ef8aa49960203826287aa3805a
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-05-25 15:10:33 -0300

    Update Image.tsx

--- challenge_image_gallery/src/components/Image.tsx
+++ challenge_image_gallery/src/components/Image.tsx
@@ -1,9 +1,24 @@
+import { useModal } from "../hooks/useModal";
+import ImageDetail from "./ImageDetail";
+import Modal from "./Modal/Modal";
+
 const Image = (props:any) => {
+
+    const [active, OpenModal, CloseModal] = useModal(false);
+
+    
+const getUrl = (data) => {
+    console.log(data.target)
+}
+
     return ( 
         <>
-        <a href={props.img.urls.regular}>
-            <img className=" h-full py-2 object-cover rounded-3xl px-1" src={props.img.urls.small} alt={props.img.alt_description} />
+        <a onClick={OpenModal}>
+            <img className=" h-full py-2 object-cover rounded-3xl px-1 hover:drop-shadow-2xl" src={props.img.urls.small} alt={props.img.alt_description} />
         </a>
+        <Modal active={active} closeModal={CloseModal}>
+            <ImageDetail data={props}/>
+        </Modal>
         </>
      );
 }
