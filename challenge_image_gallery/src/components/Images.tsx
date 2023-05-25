5f82368adeb74d0402f95c6529cb348a269e0dbf
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-05-25 15:10:56 -0300

    Update Images.tsx

--- challenge_image_gallery/src/components/Images.tsx
+++ challenge_image_gallery/src/components/Images.tsx
@@ -3,9 +3,6 @@ import Image from './Image'
 import { ImageContext } from '../App';
 import Find from './Find';
 
-
-
-
 function Images() {
 
     const [keyword] = useContext(ImageContext)
@@ -22,7 +19,6 @@ function Images() {
     return (
         <>
             {images.length > 0 ? <Find text={`Find: ${keyword}`}/> : <Find text={`Not Match`}/>}
-            {/* {images.length > 0 ? <h1>Find: {keyword}</h1> : <h1>Not Match</h1>} */}
             <div className="w-[80%] grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 my-2 mx-auto">
                 {(images && images.map((e, i) => <Image key={i} img={e} />))}
             </div>
