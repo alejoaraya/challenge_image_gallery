e0f7edb3b73cbe289a9fb1be74c522d0773ce544
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-05-25 16:08:31 -0300

    update modals

--- challenge_image_gallery/src/components/ImageDetail.tsx
+++ challenge_image_gallery/src/components/ImageDetail.tsx
@@ -2,8 +2,10 @@ const ImageDetail = (props: any) => {
 
     return (
         <>
-            <div className=" flex flex-row">
-                <img className=" h-full  rounded-s-3xl  hover:drop-shadow-2xl" src={props.data.img.urls.small} alt={props.data.img.alt_description} />
+            <div className=" flex flex-col lg:flex-row overflow-auto">
+                <div className=" sm:h-[100%] flex justify-center">
+                    <img className="rounded-s-2xl sm:h-[60%] lg:rounded-s-3xl  " src={props.data.img.urls.small} alt={props.data.img.alt_description} />
+                </div>
                 <div className="w-full">
                     <h1 className="text-3xl pt-7 py-4 px-8">{props.data.img.alt_description}</h1>
                     <hr />
--- challenge_image_gallery/src/components/Modal/Modal.tsx
+++ challenge_image_gallery/src/components/Modal/Modal.tsx
@@ -1,12 +1,14 @@
 // import './Modal.css'
 
-const Modal = ({children,active, closeModal}) => {
-    return (  
+const Modal = ({ children, active, closeModal }) => {
+    return (
         <article className={`${(!active && 'hidden')} flex fixed  z-10 top-0 left-0 w-full min-h-full bg-black/[.75] justify-center items-center`}>
-        <div className='relative w-[60%] h-[60%] bg-white rounded-3xl'>
-            <button className='absolute top-1 right-5 text-2xl' onClick={closeModal}>X</button>
-            {children}
-        </div>
+            <div className='relative w-[95%] h-[60%] top-0 bg-white rounded-3xl xl:w-[65%]'>
+                <div className=" absolute h-[30px] w-[30px] bg-gray-200 rounded-3xl top-1 right-5">
+                    <button className='font-bold  absolute right-2 top-1 text-black' onClick={closeModal}>X</button>
+                </div>
+                {children}
+            </div>
         </article>
     );
 }
